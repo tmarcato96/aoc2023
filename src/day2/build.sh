3e24ab1ff5825cb0024e98671f710ee884474a90
tmarcato <tmarcato@ethz.ch>
2023-12-12 16:24:01 +0100

    Added Solution to Day 2 Part 1

---
+++ src/day2/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+clang++ -Wall -std=c++11 -o day2 day2.cpp
---
+++ src/day2/day2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <cctype>
+
+int main(int argc, char* argv[]) {
+    std::string line, output;
+    std::ifstream input_file(argv[1]);
+    std::regex spaceRegex(" ");
+    std::string colors[3] = {"red", "blue", "green"};
+    std::vector<int> balls;
+    int lineCount = 0, sum=0;
+
+    if (input_file.is_open()) {
+        while(std::getline(input_file, line)) {
+            lineCount += 1;
+            // Remove spaces
+            line = std::regex_replace(line, spaceRegex, "");
+            size_t posColor = 0;
+            std::string ballCount;
+            int ballCountInt = 0;
+            for (size_t i=0; i<3; ++i) {
+                while(posColor!=std::string::npos) {
+                    posColor = line.find(colors[i], posColor+1);
+                    for (size_t j=2; j>=1; --j) {
+                    if (isdigit(line[posColor-j])) ballCount += line[posColor-j];
+                }
+                if (posColor!=std::string::npos) {
+                    int t = std::stoi(ballCount);
+                    if (t > ballCountInt) ballCountInt = t;
+                }
+                ballCount.clear();
+                
+                }
+                posColor = 0;
+                balls.push_back(ballCountInt);
+                ballCountInt = 0;
+            }
+            if (balls[0]<=12 && balls[1]<=14 && balls[2]<=13) sum += lineCount;
+
+            balls.clear();
+        }
+        std::cout << "The result is: " << sum << "\n";
+    }
+    else {
+        std::cout << "Unable to open file!\n"; 
+    }
+}
